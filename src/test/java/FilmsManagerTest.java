20425d720cccd68d4c3c0ac7edbdd26ec3c97b40
ElenaLilu <elena.imyarekova1@gmail.com>
2023-08-11 16:43:49 +0300

    pom.xml and src/

--- src/test/java/FilmsManagerTest.java
+++ src/test/java/FilmsManagerTest.java
@@ -17,13 +17,9 @@ public class FilmsManagerTest {
         manager.add(film1);
         manager.add(film2);
         manager.add(film3);
-        manager.add(film4);
-        manager.add(film5);
-        manager.add(film6);
-        manager.add(film7);
 
         String[] actual = manager.findAll();
-        String[] expected = {film1, film2, film3, film4, film5, film6, film7};
+        String[] expected = {film1, film2, film3};
 
         Assertions.assertArrayEquals(expected, actual);
     }
